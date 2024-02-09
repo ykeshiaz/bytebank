699d6bc467fe576dc3164b63aed8ac96b40c6974
Ykeshia Zamore <yzamore@gmail.com>
2024-02-09 09:43:44 -0600

    shifted files to main directory

--- capstone-meal-planner/src/main/java/com/techelevator/uploads/UploadProvider.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.uploads;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadProvider
-{
-    public String uploadFile(MultipartFile file, String userName, String fileName ) throws Exception;
-}
-
